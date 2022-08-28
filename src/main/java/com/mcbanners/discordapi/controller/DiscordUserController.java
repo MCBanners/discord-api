e195fe0fac1e6eb036389c94486ee2542003ce5f
darbyjack <admin@glaremasters.me>
2022-08-28 15:56:55 -0500

    We are getting users, not discord itself.

--- src/main/java/com/mcbanners/discordapi/controller/DiscordUserController.java
+++ src/main/java/com/mcbanners/discordapi/controller/DiscordUserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("discord")
+@RequestMapping("user")
 public class DiscordUserController {
     private final DiscordUserService discordUserService;
 
