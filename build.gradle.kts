539d201d8914fdac5e00db29af5fee07b61e081d
Glare <admin@glaremasters.me>
2025-04-27 15:10:56 -0500

    chore(deps): update deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("org.springframework.boot") version "3.3.2"
-    id("io.spring.dependency-management") version "1.1.6"
-    id("com.github.ben-manes.versions") version "0.51.0"
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("com.github.ben-manes.versions") version "0.52.0"
     id("java")
 }
 
@@ -20,16 +20,16 @@ repositories {
 }
 
 dependencies {
-    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2023.0.3"))
+    implementation(platform("org.springframework.cloud:spring-cloud-dependencies:2024.0.1"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
-    implementation("com.github.ben-manes.caffeine:caffeine:3.1.8")
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
     implementation("dev.triumphteam:triumph-cmd-jda-slash:2.0.0-SNAPSHOT")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
-    implementation("com.google.guava:guava:33.2.1-jre")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.0")
+    implementation("com.google.guava:guava:33.4.8-jre")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 }
